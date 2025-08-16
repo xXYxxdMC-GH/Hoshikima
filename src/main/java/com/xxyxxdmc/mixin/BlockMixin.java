ffc68b50ad950289ef9b77f276e8374958376675
xXYxxdMC-GH <xXYxxdMC@outlook.com>
2025-08-16 20:12:21 +0800

    Update

--- src/main/java/com/xxyxxdmc/mixin/BlockMixin.java
+++ src/main/java/com/xxyxxdmc/mixin/BlockMixin.java
@@ -2,14 +2,18 @@ package com.xxyxxdmc.mixin;
 
 import com.xxyxxdmc.function.ChainMineState;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.intprovider.IntProvider;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(Block.class)
 public class BlockMixin {
@@ -20,11 +24,20 @@ public class BlockMixin {
             ci.cancel();
         }
     }
-    @Inject(method = "dropExperience(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;I)V", at = @At("HEAD"), cancellable = true)
-    private static void onDropExperience(ServerWorld world, BlockPos pos, int size, CallbackInfo ci) {
+    @Inject(
+        method = "dropExperienceWhenMined",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/block/Block;dropExperience(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;I)V"
+        ),
+        cancellable = true
+    )
+    private static void onDropExperience(ServerWorld world, BlockPos pos, ItemStack stack, int amount, CallbackInfo ci) {
         if (ChainMineState.isChainMining()) {
-            ChainMineState.addCapturedXp(size);
+            ChainMineState.addCapturedXp(amount);
             ci.cancel();
         }
     }
+
 }
+
