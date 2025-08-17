0f8dc85d23101b563702f9f416e7239931a309ef
xXYxxdMC-GH <xXYxxdMC@outlook.com>
2025-08-17 19:02:29 +0800

    Update

---
+++ src/main/java/com/xxyxxdmc/config/CommonValue.java
@@ -0,0 +1,18 @@
+package com.xxyxxdmc.config;
+
+import java.awt.Color;
+import java.util.List;
+
+public class CommonValue {
+    public static final List<Integer> colors = List.of(
+        Color.RED.getRGB(),
+        Color.GREEN.getRGB(),
+        Color.BLUE.getRGB(),
+        Color.YELLOW.getRGB(),                
+        0xFF800080, // Purple
+        Color.CYAN.getRGB(),             
+        Color.WHITE.getRGB(),             
+        Color.BLACK.getRGB(),
+        0xFFFF9CFC
+    );
+}       
